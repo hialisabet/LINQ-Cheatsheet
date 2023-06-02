6cd5ccc4c59f0e43d3786d97e8c9d6adce05d404
Ali Sabet <hialisabet@gmail.com>
2023-06-02 16:44:03 +0330

    Add "Count" Method

--- LINQ Cheatsheet/Program.cs
+++ LINQ Cheatsheet/Program.cs
@@ -71,6 +71,55 @@ namespace LINQ_Cheatsheet
             List<int> emptyListAll = new();
             bool allEmptyList = emptyListAll.All(n => true);
             // Output: True
+
+            // Example 7: LINQ All check not null
+            List<Person> persons = new()
+            {
+                new Person { Name = "Alice" },
+                new Person { Name = "Bob" },
+                new Person { Name = "Charlie" },
+                new Person { Name = null },
+                new Person { Name = "Eve" }
+            };
+            bool allNamesNotNull = persons.All(person => person.Name != null);
+            // Output: False
+
+            #endregion
+
+            #region Count
+            // Example 1: Count all numbers in the array
+            int[] numbers1 = { 1, 2, 3, 4, 5 };
+            int countNumbers1 = numbers1.Count();
+            // Output: 5
+
+            // Example 2: Count all names in the array
+            string[] names = { "Alice", "Bob", "Charlie" };
+            int countNames = names.Count();
+            // Output: 3
+
+            // Example 3: Count elements in an empty list
+            List<int> emptyList = new();
+            int countEmptyList = emptyList.Count();
+            // Output: 0
+
+            // Example 4: Count names starting with 'B' in a list
+            List<string> namesStartingWithB = new() { "Bob", "Bella", "Ben" };
+            int countNamesStartingWithB = namesStartingWithB.Count(name => name.StartsWith("B"));
+            // Output: 2
+
+            // Example 5: Count numbers greater than three in a list
+            List<int> numbersGreaterThanThree = new() { 4, 5, 6, 7, 8 };
+            int countNumbersGreaterThanThree = numbersGreaterThanThree.Count(n => n > 3);
+            // Output: 4
+
+            // Example 6: Count non-null names in a list
+            List<string> namesNull = new() { "Alice", null, "Bob", null, "Charlie" };
+            int countNonNullNames = namesNull.Count(name => name != null);
+            // Output: 3
+            #endregion
+
+            #region Contains
+
             #endregion
         }
     }
